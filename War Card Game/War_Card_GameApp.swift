e50eea2a2125ee2cb98f4f81f904a7c57d56227d
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2024-04-06 15:47:52 -0400

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Panchi on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
